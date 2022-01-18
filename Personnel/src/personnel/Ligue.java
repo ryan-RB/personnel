007f70795edec8604cd6341568d9ae913342db38
Ryan <ryanmixt.rb@gmail.com>
2022-01-18 13:12:19 +0100

    MàJ Date
    Gestion de la date de départ et de celle d’arrivée.
    
    Co-Authored-By: abane1 <90394348+abane1@users.noreply.github.com>
    Co-Authored-By: Redjan29 <74569624+Redjan29@users.noreply.github.com>

--- Personnel/src/personnel/Ligue.java
+++ Personnel/src/personnel/Ligue.java
@@ -108,9 +108,9 @@ public class Ligue implements Serializable, Comparable<Ligue>
 	 * @return l'employé créé. 
 	 */
 
-	public Employe addEmploye(String nom, String prenom, String mail, String password)
+	public Employe addEmploye(String nom, String prenom, String mail, String password, String dateArrive, String dateDepart)
 	{
-		Employe employe = new Employe(this.gestionPersonnel, this, nom, prenom, mail, password);
+		Employe employe = new Employe(this.gestionPersonnel, this, nom, prenom, mail, password, dateArrive, dateDepart);
 		employes.add(employe);
 		return employe;
 	}
