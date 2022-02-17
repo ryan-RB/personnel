a66d76e2bc94b546826deebbd498bcad51698e5c
Ryan <ryanmixt.rb@gmail.com>
2022-02-17 22:20:33 +0100

    Update testLigue.java

--- src/testsUnitaires/testLigue.java
+++ src/testsUnitaires/testLigue.java
@@ -12,15 +12,65 @@ class testLigue
 	@Test
 	void createLigue() throws SauvegardeImpossible
 	{
-		Ligue ligue = gestionPersonnel.addLigue("FlÃ©chettes");
-		assertEquals("FlÃ©chettes", ligue.getNom());
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		assertEquals("Fléchettes", ligue.getNom());
 	}
 
 	@Test
 	void addEmploye() throws SauvegardeImpossible
 	{
-		Ligue ligue = gestionPersonnel.addLigue("FlÃ©chettes");
-		Employe employe = ligue.addEmploye("Bouchard", "GÃ©rard", "g.bouchard@gmail.com", "azerty", null, null); 
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty", null, null); 
 		assertEquals(employe, ligue.getEmployes().first());
 	}
+	
+	@Test
+	void getNom() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Pingu");
+		assertEquals("Pingu", ligue.getNom());
+	}
+	
+	@Test
+	void setNom() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Pingu");
+		ligue.setNom("Fléchettes");
+		assertEquals("Fléchettes", ligue.getNom());
+	}
+	
+	@Test
+	void toStringLigue() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Pingu");
+		assertEquals("Pingu", ligue.toString());
+	}
+	
+	
+	@Test
+	void setAdmin() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Pingu");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty", null, null);
+		ligue.setAdministrateur(employe);
+		assertEquals(employe, ligue.getAdministrateur());
+	}
+	
+	@Test
+	void getAdmin() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Pingu");
+		assertEquals("root   null null (super-utilisateur)", ligue.getAdministrateur().toString());
+	}
+	
+	@Test
+	void remove() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Pingu");
+		int initSize = gestionPersonnel.getLigues().size();
+		ligue.remove();
+		assertEquals(initSize - 1, gestionPersonnel.getLigues().size());
+	}
+
+
 }
